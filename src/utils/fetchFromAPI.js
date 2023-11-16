309db582812ea02e27c7c0880ba928d1aacb53a7
Abolfazl Janmohammadloo <abolfazljanmo@gmail.com>
2023-11-16 18:02:24 +0330

    first commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': "7d1eeb106fmshcff7160b14a12edp1c3822jsnf7276c2e757d",
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
